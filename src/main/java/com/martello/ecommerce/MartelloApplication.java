4a6eba2862cf2c0ed81c3d7ae73654e9a58fb7af
Nikkk28 <marthanikshith.28@gmail.com>
2025-05-13 22:18:38 +0530

    initial commit

---
+++ src/main/java/com/martello/ecommerce/MartelloApplication.java
@@ -0,0 +1,12 @@
+package com.martello.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartelloApplication.class, args);
+	}
+}
---
+++ src/test/java/com/martello/ecommerce/MartelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martello.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
